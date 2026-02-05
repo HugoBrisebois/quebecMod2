df1e989fd5fdd916af2c795d197d27c3729186c7
hugo brisebois <134088623+HugoBrisebois@users.noreply.github.com>
2026-02-04 19:56:53 -0500

    changing the way to register an item and getting started on adding a new item

--- src/main/java/github/com/quebec/ModItems.java
+++ src/main/java/github/com/quebec/ModItems.java
@@ -1,27 +1,31 @@
 package github.com.quebec;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import java.util.function.Function;
 
 public class ModItems {
-    public static <GenericItem extends Item> GenericItem register(String name, Function<Item.Settings, GenericItem> itemFactory, Item.Settings settings) {
-        // create the identifier
-        Identifier id = Identifier.of(Quebec.MOD_ID, name);
 
-        // create item instance
-        GenericItem item = itemFactory.apply(settings);
-
-        // register the item
-        Registry.register(Registries.ITEM, id, item);
+    public static final Item MapleSyrup = registerItem(name = "Maple Syrup", new Item(new Item.Settings()));
 
 
-        return item;
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(Quebec.MOD_ID, name), item);
     }
 
+    public static void registerModItems() {
+        Quebec.LOGGER.info(" register a mod item" + Quebec.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add((MapleSyrup));
+        });
+    }
 
     public static void initialize() {
+
     }
 }
