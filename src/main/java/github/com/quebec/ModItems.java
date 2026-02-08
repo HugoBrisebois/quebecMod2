155f90d1a4c7b9efdca4bdbedb824ac27a04fa97
hugo brisebois <134088623+HugoBrisebois@users.noreply.github.com>
2026-02-08 09:30:01 -0500

    trying to get a new item to register

--- src/main/java/github/com/quebec/ModItems.java
+++ src/main/java/github/com/quebec/ModItems.java
@@ -6,22 +6,18 @@ import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import java.util.function.Function;
 
 public class ModItems {
 
-    public static final Item MapleSyrup = registerItem(name = "Maple Syrup", new Item(new Item.Settings()));
-
+    public static final Item MapleSyrup = registerItem("maple_syrup", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(Quebec.MOD_ID, name), item);
     }
 
     public static void registerModItems() {
-        Quebec.LOGGER.info(" register a mod item" + Quebec.MOD_ID);
-
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
-            entries.add((MapleSyrup));
+            entries.add(MapleSyrup);
         });
     }
 
--- src/main/java/github/com/quebec/Quebec.java
+++ src/main/java/github/com/quebec/Quebec.java
@@ -1,9 +1,14 @@
 package github.com.quebec;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Quebec implements ModInitializer {
     public static final String MOD_ID = "quebec";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
 
     @Override
     public void onInitialize() {
